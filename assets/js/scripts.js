a5a347416aae175ece9b4b27ea57e8a4e0256d0d
FrozenPyromancer <frozenpyromancer@gmail.com>
2017-07-26 22:05:28 +0200

    Create scripts.js

---
+++ assets/js/scripts.js
@@ -0,0 +1,4 @@
+var colorClasses = ['red','orange', 'blue', 'green']; //add as many classes as u want
+var randomnumber = Math.floor(Math.random()*classes.length);
+
+$('body').addClass(colorClasses[randomnumber]);
