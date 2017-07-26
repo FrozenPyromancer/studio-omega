1849124dc012662fd7edb2711609d2aa63949002
FrozenPyromancer <frozenpyromancer@gmail.com>
2017-07-26 22:23:26 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
-var colorClasses = ['red','orange', 'blue', 'green']; //add as many classes as u want
-var randomnumber = Math.floor(Math.random()*classes.length);
-
-$('body').addClass(colorClasses[randomnumber]);
+$(document).ready(function(){
+  
+  // Add class to Body
+  var colorClasses = ['red','orange', 'blue', 'green']; //add as many classes as u want
+  var colorRandomnumber = Math.floor(Math.random()*classes.length) + 1;
+  $('body').addClass(colorClasses[colorRandomnumber]); 
+  
+  // ***
+});
